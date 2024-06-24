957a06e901d31067e4c6ceb3c4db53e69e1d0454
花木凋零成兰 <18570354653@163.com>
2024-06-24 13:55:40 +0800

    5.A+B问题VII

---
+++ src/com/kamacoder/k_5_AAndBQuestionVII.java
@@ -0,0 +1,23 @@
+package com.kamacoder;
+
+import java.util.Scanner;
+
+/**
+ * @author 花木凋零成兰
+ * @title k_5_AAndBQuestionVII
+ * @date 2024/6/24 13:43
+ * @package com.kamacoder
+ * @description 5. A+B问题VII
+ */
+public class k_5_AAndBQuestionVII {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b;
+        while (in.hasNext()) {
+            a = in.nextInt();
+            b = in.nextInt();
+            System.out.println(a + b);
+            System.out.println();
+        }
+    }
+}
